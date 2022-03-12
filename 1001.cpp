560569ee3049201fc3c632720ed46cb7549554b9
Rodrigo Centeno <rodrigocenteno29@gmail.com>
2022-03-12 15:47:15 -0300

    teste inicial

---
+++ 1001.cpp
@@ -0,0 +1,20 @@
+/*
+Data: 06/03/2022
+Autor: rodrigocenteno29@gmail.com
+Objetivo: Beecrowd 1005
+*/
+#include <studio.h>
+int main()
+{
+    // declaração de variáveis
+    int A, B, soma;
+    // entrada de dados
+    scanf("%d", &A);
+    scanf("%d", &B);
+    // processamento
+    soma = A + B;
+    // saida de dados
+    printf("X = %d\n", soma);
+    
+    return 0;
+}
